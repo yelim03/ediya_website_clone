fb396ef7534fe8c9ce17da7bc1f125b1233d7fc5
yelim03 <yelim5570@gmail.com>
2023-12-26 17:24:10 +0900

    231226

---
+++ script.js
@@ -0,0 +1,177 @@
+// $(function () {
+//   //Header Scroll Change
+//   $(window).scroll(function () {
+//     if ($(window).scrollTop() > 50) {
+//       $("header").addClass("active");
+//     } else {
+//       $("header").removeClass("active");
+//     }
+//   });
+// });
+
+//Main Slide
+const slide = document.querySelector(".slide");
+let slideWidth = slide.clientWidth;
+const prevBtn = document.querySelector(".slidePrevButton");
+const nextBtn = document.querySelector(".slideNextButton");
+
+let slideItems = document.querySelectorAll(".slideItem");
+const maxSlide = slideItems.length;
+
+let currSlide = 1;
+
+// 페이지네이션 생성
+const pagination = document.querySelector(".slidePagination");
+
+for (let i = 0; i < maxSlide; i++) {
+  if (i === 0) pagination.innerHTML += `<li class="active">•</li>`;
+  else pagination.innerHTML += `<li>•</li>`;
+}
+
+const paginationItems = document.querySelectorAll(".slidePagination > li");
+
+const startSlide = slideItems[0];
+const endSlide = slideItems[slideItems.length - 1];
+const startElem = document.createElement("div");
+const endElem = document.createElement("div");
+
+endSlide.classList.forEach((c) => endElem.classList.add(c));
+endElem.innerHTML = endSlide.innerHTML;
+
+startSlide.classList.forEach((c) => startElem.classList.add(c));
+startElem.innerHTML = startSlide.innerHTML;
+
+slideItems[0].before(endElem);
+slideItems[slideItems.length - 1].after(startElem);
+
+slideItems = document.querySelectorAll(".slideItem");
+//
+let offset = slideWidth + currSlide;
+slideItems.forEach((i) => {
+  i.setAttribute("style", `left: ${-offset}px`);
+});
+
+function nextMove() {
+  currSlide++;
+
+  if (currSlide <= maxSlide) {
+    const offset = slideWidth * currSlide;
+    slideItems.forEach((i) => {
+      i.setAttribute("style", `left: ${-offset}px`);
+    });
+    paginationItems.forEach((i) => i.classList.remove("active"));
+    paginationItems[currSlide - 1].classList.add("active");
+  } else {
+    currSlide = 0;
+    let offset = slideWidth * currSlide;
+    slideItems.forEach((i) => {
+      i.setAttribute("style", `transition: ${0}s; left: ${-offset}px`);
+    });
+    currSlide++;
+    offset = slideWidth * currSlide;
+    setTimeout(() => {
+      slideItems.forEach((i) => {
+        i.setAttribute("style", `transition: ${0.15}s; left: ${-offset}px`);
+      });
+    }, 0);
+    paginationItems.forEach((i) => i.classList.remove("active"));
+    paginationItems[currSlide - 1].classList.add("active");
+  }
+}
+function prevMove() {
+  currSlide--;
+  if (currSlide > 0) {
+    const offset = slideWidth * currSlide;
+    slideItems.forEach((i) => {
+      i.setAttribute("style", `left: ${-offset}px`);
+    });
+
+    paginationItems.forEach((i) => i.classList.remove("active"));
+    paginationItems[currSlide - 1].classList.add("active");
+  } else {
+    currSlide = maxSlide + 1;
+    let offset = slideWidth * currSlide;
+
+    slideItems.forEach((i) => {
+      i.setAttribute("style", `transition: ${0}s; left: ${-offset}px`);
+    });
+    currSlide--;
+    offset = slideWidth * currSlide;
+    setTimeout(() => {
+      slideItems.forEach((i) => {
+        i.setAttribute("style", `transition: ${0.15}s; left: ${-offset}px`);
+      });
+    }, 0);
+
+    paginationItems.forEach((i) => i.classList.remove("active"));
+    paginationItems[currSlide - 1].classList.add("active");
+  }
+}
+
+nextBtn.addEventListener("click", () => {
+  nextMove();
+});
+prevBtn.addEventListener("click", () => {
+  prevMove();
+});
+
+window.addEventListener("resize", () => {
+  slideWidth = slide.clientWidth;
+});
+
+for (let i = 0; i < maxSlide; i++) {
+  paginationItems[i].addEventListener("click", () => {
+    currSlide = i + 1;
+    const offset = slideWidth * currSlide;
+    slideItems.forEach((i) => {
+      i.setAttribute("style", `left: ${-offset}px`);
+    });
+    paginationItems.forEach((i) => i.classList.remove("active"));
+    paginationItems[currSlide - 1].classList.add("active");
+  });
+}
+
+let startPoint = 0;
+let endPoint = 0;
+
+slide.addEventListener("mousedown", (e) => {
+  startPoint = e.pageX;
+});
+slide.addEventListener("mouseup", (e) => {
+  endPoint = e.pageX;
+  if (startPoint < endPoint) {
+    prevMove();
+  } else if (startPoint > endPoint) {
+    nextMove();
+  }
+});
+
+// 모바일 터치 이벤트 (스와이프)
+slide.addEventListener("touchstart", (e) => {
+  startPoint = e.touches[0].pageX;
+});
+slide.addEventListener("touchend", (e) => {
+  endPoint = e.changedTouches[0].pageX;
+  if (startPoint < endPoint) {
+    prevMove();
+  } else if (startPoint > endPoint) {
+    nextMove();
+  }
+});
+
+// 기본적으로 슬라이드 루프 시작하기
+let loopInterval = setInterval(() => {
+  nextMove();
+}, 3000);
+
+// 슬라이드에 마우스가 올라간 경우 루프 멈추기
+slide.addEventListener("mouseover", () => {
+  clearInterval(loopInterval);
+});
+
+// 슬라이드에서 마우스가 나온 경우 루프 재시작하기
+slide.addEventListener("mouseout", () => {
+  loopInterval = setInterval(() => {
+    nextMove();
+  }, 3000);
+});
